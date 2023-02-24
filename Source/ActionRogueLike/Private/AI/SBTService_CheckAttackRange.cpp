5e9405945b545c7e6b2aa549e71ba2ddc1eb0aeb
kmy9317 <kimmin9632@gmail.com>
2023-02-24 20:39:49 +0900

    Craete Ranged Enemy

---
+++ Source/ActionRogueLike/Private/AI/SBTService_CheckAttackRange.cpp
@@ -0,0 +1,42 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AI/SBTService_CheckAttackRange.h"
+#include "BehaviorTree/BlackboardComponent.h"
+#include "AIController.h"
+
+void USBTService_CheckAttackRange::TickNode(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds)
+{
+	Super::TickNode(OwnerComp, NodeMemory, DeltaSeconds);
+
+	// Check distance between ai pawn and target actor
+
+	UBlackboardComponent* BlackboardComp = OwnerComp.GetBlackboardComponent();
+	if (ensure(BlackboardComp))
+	{
+		AActor* TargetActor = Cast<AActor>(BlackboardComp->GetValueAsObject("TargetActor"));
+		if (TargetActor)
+		{
+			AAIController* MyController = OwnerComp.GetAIOwner();
+			if (ensure(MyController))
+			{
+				APawn* AIPawn = MyController->GetPawn();
+				if (ensure(AIPawn))
+				{
+					float Distance = FVector::Distance(TargetActor->GetActorLocation(), AIPawn->GetActorLocation());
+
+					bool bWithinRange = Distance < 2000.f;
+
+					bool bHasLOS = false;
+					if (bWithinRange)
+					{
+						bHasLOS = MyController->LineOfSightTo(TargetActor);
+					}
+
+					BlackboardComp->SetValueAsBool(AttackRangeKey.SelectedKeyName, (bWithinRange && bHasLOS));
+
+				}
+			}
+		}
+	}
+}
