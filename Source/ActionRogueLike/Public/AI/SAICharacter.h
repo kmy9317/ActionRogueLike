300d921e1aab21391f89e3b10f7063743173b877
kmy9317 <kimmin9632@gmail.com>
2023-03-10 11:02:25 +0900

    Create Thorns, Rage Actions

--- Source/ActionRogueLike/Public/AI/SAICharacter.h
+++ Source/ActionRogueLike/Public/AI/SAICharacter.h
@@ -6,6 +6,7 @@
 #include "GameFramework/Character.h"
 #include "SAICharacter.generated.h"
 
+
 class UPawnSensingComponent;
 class USAttributeComponent;
 class UUserWidget;
@@ -18,6 +19,7 @@ class ACTIONROGUELIKE_API ASAICharacter : public ACharacter
 	GENERATED_BODY()
 
 public:
+
 	ASAICharacter();
 
 protected:
@@ -27,26 +29,38 @@ protected:
 	UPROPERTY(EditDefaultsOnly, Category = "UI")
 	TSubclassOf<UUserWidget> HealthBarWidgetClass;
 
+	/* Widget to display when bot first sees a player. */
+	UPROPERTY(EditDefaultsOnly, Category = "UI")
+	TSubclassOf<UUserWidget> SpottedWidgetClass;
+
+	/* Material parameter for Hitflashes */
 	UPROPERTY(VisibleAnywhere, Category = "Effects")
 	FName TimeToHitParamName;
 
+	/* Key for AI Blackboard 'TargetActor' */
+	UPROPERTY(VisibleAnywhere, Category = "Effects")
+	FName TargetActorKey;
+
+	UFUNCTION(BlueprintCallable, Category = "AI")
 	void SetTargetActor(AActor* NewTarget);
 
+	UFUNCTION(BlueprintCallable, Category = "AI")
+	AActor* GetTargetActor() const;
+
 	virtual void PostInitializeComponents() override;
-
-	UPROPERTY(VisibleAnywhere, Category = "Component")
-	UPawnSensingComponent* PawnSensingComp;
-
-	UPROPERTY(VisibleAnywhere, Category = "Component")
-	USAttributeComponent* AttributeComp;
-
-	UPROPERTY(VisibleAnywhere, Category = "Component")
-	USActionComponent* ActionComp;
 
 	UFUNCTION()
 	void OnHealthChanged(AActor* InstigatorActor, USAttributeComponent* OwningComp, float NewHealth, float Delta);
 
+	UPROPERTY(VisibleAnywhere, Category = "Components")
+	UPawnSensingComponent* PawnSensingComp;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
+	USAttributeComponent* AttributeComp;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
+	USActionComponent* ActionComp;
+
 	UFUNCTION()
 	void OnPawnSeen(APawn* Pawn);
-
 };
